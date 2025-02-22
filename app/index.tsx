6bb3fe831bddf57412578956fc9ad94534ef8736
Niraj Bagdi <bagdiniraj22@gmail.com>
2025-02-22 16:56:28 +0530

    add tinder-like swipe support

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import { View, ImageBackground, TouchableOpacity } from 'react-native';
 import { CardItem } from './components';
 import styles from '../assets/styles';
 import DEMO from '../assets/data';
 import Icon from './components/Icon';
 
+import CardStack from './components/CardStack';
+import Card from './components/Card';
+
 const Index = () => {
+    const [swiper, setSwiper] = useState<CardStack | null>(null);
+
     return (
         <ImageBackground
             source={require('../assets/images/bg.png')}
@@ -21,15 +27,23 @@ const Index = () => {
             </TouchableOpacity>
 
             <View style={styles.containerHome}>
-                {DEMO.map((item) => (
-                    <CardItem
-                        key={item.id}
-                        hasActions
-                        image={item.image}
-                        name={item.name}
-                        description={item.description}
-                    />
-                ))}
+                <CardStack
+                    loop
+                    verticalSwipe={false}
+                    renderNoMoreCards={() => null}
+                    ref={(newSwiper): void => setSwiper(newSwiper)}
+                >
+                    {DEMO.map((item) => (
+                        <Card key={item.id}>
+                            <CardItem
+                                hasActions
+                                image={item.image}
+                                name={item.name}
+                                description={item.description}
+                            />
+                        </Card>
+                    ))}
+                </CardStack>
             </View>
         </ImageBackground>
     );
